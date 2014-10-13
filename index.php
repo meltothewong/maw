e982102cb81eb07a28c41c9f5473ebdfdc25ec56
Melissa Wong <melissa.wong89@gmail.com>
2014-10-13 12:48:38 -0700

    MAW revisions 1

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             <div class="container">
                 <section>
                     <h1><span>Let's Talk Trash</span></h1>
-                    <p>According to a 2012 report from the Environmental Protection Agency (EPA), Americans generate <a href="http://www.epa.gov/osw/nonhaz/municipal/pubs/2012_msw_fs.pdf" target="_blank">251 million tons of trash per year</a>. Our per capita trash disposal rate is a whopping 4.6 pounds per person, per day. Sixty-five percent come from our homes, while 35 percent come from commercial locations and institutions like restaurants, schools, hospitals and yes &mdash; businesses.</p>
+                    <p>According to a 2012 report from the Environmental Protection Agency (EPA), Americans generate <a href="http://www.epa.gov/osw/nonhaz/municipal/pubs/2012_msw_fs.pdf" target="_blank">251 million tons of trash per year</a>. Our per capita trash disposal rate is a whopping 4.6 pounds per person, per day. 65% come from our homes, while 35% come from commercial locations and institutions like restaurants, schools, hospitals and yes &mdash; our businesses.</p>
 
                     <p>Where does it all end up? About 12% goes to incinerators, 34% gets recycled, and 54% gets buried in landfills.</p>
 
@@ -40,14 +40,14 @@
                 <section>
                     <h1><span>Marketers Aren&#8217;t Helping</span></h1>
                     <p>As Marketers, we are sometimes under a strange social pressure to create a lot of &quot;stuff&quot; &mdash; t-shirts, pens, hats, bags, stress balls, and countless other tchotchkes with our logos on them (aka, promotional products, or &quot;swag&quot;).</p>
-                    <p>We make these things because we hope that they will promote our brand and lead to customers in our pipelines. But do they actually help us achieve our goals? And even if they did, is the impact they have on our planet worth it?</p> 
+                    <p>We make these things because we hope that they will promote our brand and lead to customers in our pipelines. But do they actually help us achieve our goals? And even if they did, is the impact they have on our planet truly worth it?</p> 
                 </section>
                 <section>
                     <h1><span>Three dirty little secrets</span></h1>
                     <p>Here are three dirty little secrets that the tchotchke industry doesn&#8217;t want you to know:</p>
                     <ol><li><p>1. Roughly 2/3 of the marketing materials you produce end up in a hotel trash can before your target customer even leaves the conference.</p></li> 
                     <li><p>2. People will not, in fact, buy your product because you gave them a branded flashlight keychain.</p></li>
-                    <li><p>3. People are more likely to remember <a href="http://psycnet.apa.org/journals/psp/104/2/199/" target="_blank">positive experiences</a> shared with other humans, rather than objects.</p></li>
+                    <li><p>3. People are more likely to remember <a href="http://www.psychologytoday.com/files/attachments/2557/howellpcheliniyer2012the-preference-experiences-over-possessions.pdf" target="_blank">positive experiences</a> shared with other humans, rather than objects.</p></li>
                     <ol>
                 </section>
                 <section>
@@ -55,18 +55,18 @@
                     <p>Most promotional products are made using cheap plastics or other non-biodegradable material that is inexpensive to produce. In addition &mdash; let&#8217;s be honest here &mdash; many of these items are really kind of unnecessary and not very useful. When we make a low-quality item that no one actually needs, it is more likely to get thrown away. Here&#8217;s what I propose:</p>
                     <ol><li><p>1. Let&#8217;s stop feeling like we &quot;have to&quot; make promotional items and think about alternatives &mdash; maybe we could offer consumable goods or services instead?</li></p>
                     <li><p>2. If we really need to make something, let&#8217;s look for products that can be recycled and or that are biodegradable.</li></p>
-                    <li><p>3. Let&#8217;s try to only make high-quality items that people will actually use, and let&#8217;s make less of them.</li></p>
+                    <li><p>3. Let&#8217;s try to only make high-quality items that people will actually use, and let&#8217;s make fewer of them.</li></p>
                     </ol>
                 </section><a id="initiative"></a>
                 <section>
                     <h1><span>Join the Movement</span></h1>
                     <p>Marketers Against Waste is a completely unofficial organization whose sole purpose is to bring awareness to this trash issue. You can sign our lightweight and completely non-binding pledge as an individual or as a representative of your company, and you can choose to have your company&#8217;s logo included on this website&hellip; or not. It&#8217;s completely up to you.</p> 
-                    <p>We&#8217;re providing a nifty badge you can put on your website or display at your tradeshow booth to show that you also support making sustainable promotional item choices.</a>Together, we can reduce waste and lessen the impact that our industry has on the planet!</p> 
+                    <p>We&#8217;re providing a nifty badge you can put on your website or display at your tradeshow booth to show that you also support making sustainable promotional item choices.</a>Together, we can reduce waste and lessen the impact that our businesses have on the planet!</p> 
                 </section><a id="pledge"></a>
             </div>
             <div class="container-wide">
                 <section>
-                    <blockquote>&quot;As a Marketer, I promise to think about our planet before I create swag. I will try to limit the amount of trash my organization creates, and if I do need to produce branded marketing materials, I will look for products that are useful, sustainable and less likely to end up in a landfill.&quot;</blockquote>
+                    <blockquote>&quot;As a Marketer, I promise to think about our planet before I create promotional items. I will try to limit the amount of trash my organization creates, and if I do need to produce branded marketing materials, I will look for products that are useful, sustainable and less likely to end up in a landfill.&quot;</blockquote>
                 </section>
                 <section class="tweet-machine center box">
                     <div class="tweet-machine-sentence">
