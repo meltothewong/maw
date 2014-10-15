d6617972ce88f216afd9dd228e17c92ec916b334
Melissa Wong <melissa.wong89@gmail.com>
2014-10-14 18:01:30 -0700

    More advocates 2

--- advocates/index.php
+++ advocates/index.php
@@ -3,6 +3,12 @@
             <div class="container-wide center">
                 <section>
                     <div class="row logo"><img src="http://s3.amazonaws.com/fjds/member/original/694002/piston-logo-transparent.png?1389904914"></div>
+                    <div class="row logo"><img src="/images/apcera-logo.png"></div>
+                    <div class="row logo"><img src="/images/exaqueo-logo.png"></div>
+                </section>
+            </div>
+                </section>
+            </div>
                 </section>
             </div>
         </div>
